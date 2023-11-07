79f79a6652ef78cd4368a24227aaad7e3d0f336d
chrillah <ch.ronnberg@gmail.com>
2023-11-07 19:22:30 +0100

    it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { storage } from './firebase'
+import { ref, listAll, getDownloadURL } from 'firebase/storage'
 
-function App() {
+function App(): JSX.Element {
+    const [imageList, setImageList] = useState<string[]>([])
+    const imageListRef = ref(storage, 'images/')
+
+    useEffect(() => {
+        setImageList([])
+        listAll(imageListRef).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+    }, [])
+
     return (
         <>
             <h1>Portfolio</h1>
+            <div className="list-container">
+                {imageList.map((url, index) => (
+                    <img key={index} src={url} alt="bilder" />
+                ))}
+            </div>
         </>
     )
 }
