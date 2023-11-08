99aee2acf7ba35f72ea285e03cc0a9ce9de8e654
chrillah <ch.ronnberg@gmail.com>
2023-11-08 20:29:05 +0100

    fix

--- src/components/ContactBar.tsx
+++ src/components/ContactBar.tsx
@@ -32,9 +32,23 @@ function ContactBar() {
                     className="on-site-button"
                     onClick={() => setContactOpen(!contactOpen)}
                 >
+                    <div className={contactOpen ? 'rotate' : 'no-rotate'}>
+                        <svg
+                            id="Layer_1"
+                            data-name="Layer 1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24.98 42.21"
+                        >
+                            <polyline
+                                className="arrow"
+                                points="2.38 .89 22.6 21.11 2.38 41.32"
+                            />
+                        </svg>
+                    </div>
                     Contact
                 </button>
             </div>
+
             <div className={contactOpen ? 'contact-open' : 'contact-closed'}>
                 <ul className=""></ul>
             </div>
