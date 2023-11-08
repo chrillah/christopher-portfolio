7c6cdc7c1acfce7bc079e5377a7036bf30f09940
chrillah <ch.ronnberg@gmail.com>
2023-11-08 20:16:14 +0100

    update

--- src/pages/Hemma.tsx
+++ src/pages/Hemma.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
+import { storage } from '../firebase'
+import { ref, listAll, getDownloadURL } from 'firebase/storage'
+import { Link } from 'react-router-dom'
 function Hemma() {
+    const [imageList, setImageList] = useState<string[]>([])
+    const imageListRef = ref(storage, 'images/')
+
+    useEffect(() => {
+        setImageList([])
+        listAll(imageListRef).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+    }, [])
     return (
         <div>
-            <h1>Hemma</h1>
+            <Link to="/">HOME</Link>
+            <div>
+                {imageList.map((url, index) => (
+                    <img key={index} src={url} alt="bilder" />
+                ))}
+            </div>
         </div>
     )
 }
--- src/pages/PictureContainer.tsx
+++ src/pages/PictureContainer.tsx
@@ -1,31 +1,7 @@
-import { useEffect, useState } from 'react'
-import { storage } from '../firebase'
-import { ref, listAll, getDownloadURL } from 'firebase/storage'
-import { Link } from 'react-router-dom'
-
 function PictureContainer() {
-    const [imageList, setImageList] = useState<string[]>([])
-    const imageListRef = ref(storage, 'images/')
-
-    useEffect(() => {
-        setImageList([])
-        listAll(imageListRef).then((resp) => {
-            resp.items.forEach((item) => {
-                getDownloadURL(item).then((url) => {
-                    setImageList((prev) => [...prev, url])
-                })
-            })
-        })
-    }, [])
     return (
         <div>
             <h1>Portfolio</h1>
-            <Link to="/">HOME</Link>
-            <div className="list-container">
-                {imageList.map((url, index) => (
-                    <img key={index} src={url} alt="bilder" />
-                ))}
-            </div>
         </div>
     )
 }
