aadc823c7b93709c6b1484425b0b783ed340b4b9
chrillah <ch.ronnberg@gmail.com>
2023-11-13 10:34:20 +0100

    folders added

--- src/pages/Hemma.tsx
+++ src/pages/Hemma.tsx
@@ -4,11 +4,65 @@ import { ref, listAll, getDownloadURL } from 'firebase/storage'
 import { Link } from 'react-router-dom'
 function Hemma() {
     const [imageList, setImageList] = useState<string[]>([])
-    const imageListRef = ref(storage, 'images/')
+    const imageListTjuvgods = ref(storage, 'images/tjuvgods/')
+    const imageListEyes = ref(storage, 'images/eyes/')
+    const imageListGomoku = ref(storage, 'images/gomoku/')
+    const imageListKwitter = ref(storage, 'images/kwitter/')
+    const imageListRickAndMorty = ref(storage, 'images/rickandmorty/')
+    const imageListSweet = ref(storage, 'images/sweet/')
+    const imageListTechware = ref(storage, 'images/techware/')
 
     useEffect(() => {
         setImageList([])
-        listAll(imageListRef).then((resp) => {
+        listAll(imageListTjuvgods).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+
+        listAll(imageListEyes).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+
+        listAll(imageListGomoku).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+
+        listAll(imageListKwitter).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+
+        listAll(imageListRickAndMorty).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+
+        listAll(imageListSweet).then((resp) => {
+            resp.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url])
+                })
+            })
+        })
+
+        listAll(imageListTechware).then((resp) => {
             resp.items.forEach((item) => {
                 getDownloadURL(item).then((url) => {
                     setImageList((prev) => [...prev, url])
