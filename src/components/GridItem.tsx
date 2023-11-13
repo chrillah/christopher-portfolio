71df96710601479b9f99ab4eebe7849dd42e3fd8
chrillah <ch.ronnberg@gmail.com>
2023-11-13 21:18:04 +0100

    fix

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,79 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { StorageReference, listAll, getDownloadURL } from 'firebase/storage';
+import React, { useState, useEffect } from 'react'
+import { StorageReference, listAll, getDownloadURL } from 'firebase/storage'
+import { useNavigate  } from 'react-router-dom';
 
 interface GridItemProps {
-  imagesRef: StorageReference;
+    imagesRef: StorageReference
 }
 
 const GridItem: React.FC<GridItemProps> = ({ imagesRef }) => {
-  const [imageList, setImageList] = useState<string[]>([]);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    // let selectedProject: string = ''
+    const [imageList, setImageList] = useState<string[]>([])
+    const [currentImageIndex, setCurrentImageIndex] = useState(0)
+    const navigate = useNavigate();
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const resp = await listAll(imagesRef)
+                const urls = await Promise.all(
+                    resp.items.map(async (item) => await getDownloadURL(item))
+                )
+                setImageList(urls)
+            } catch (error) {
+                console.error('Error loading images:', error)
+            }
+        }
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const resp = await listAll(imagesRef);
-        const urls = await Promise.all(
-          resp.items.map(async (item) => await getDownloadURL(item))
-        );
-        setImageList(urls);
-      } catch (error) {
-        console.error('Error loading images:', error);
-      }
-    };
+        fetchImages()
+    }, [imagesRef])
 
-    fetchImages();
-  }, [imagesRef]);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImageIndex(
+                (prevIndex) => (prevIndex + 1) % imageList.length
+            )
+        }, 4000)
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageList.length);
-    }, 4000);
+        return () => clearInterval(intervalId)
+    }, [imageList])
 
-    return () => clearInterval(intervalId);
-  }, [imageList]);
-
-  const onFilteredListRef = async (urlParts: string[]) => {
-    for (const project of listOfProjects) {
-      const filteredArray = urlParts.filter((item) => item.includes(project));
-      if (filteredArray.length > 0) {
-        console.log(`Filtered parts for ${project}:`, filteredArray);
-        return project;
-      }
+    const onFilteredListRef = async (urlParts: string[]) => {
+        for (const project of listOfProjects) {
+            const filteredArray = urlParts.filter((item) =>
+                item.includes(project)
+            )
+            if (filteredArray.length > 0) {
+                console.log(`Filtered parts for ${project}:`, filteredArray)
+                return project
+            }
+        }
+        return ''
     }
-    return '';
-  };
 
-  const listOfProjects = [
-    'tjuvgods',
-    'kwitter',
-    'techware',
-    'rickandmorty',
-    'sweet',
-    'eyes',
-    'gomoku'
-  ];
+    const listOfProjects = [
+        'tjuvgods',
+        'kwitter',
+        'techware',
+        'rickandmorty',
+        'sweet',
+        'eyes',
+        'gomoku'
+    ]
 
-  const navigateToProject = async () => {
-    const selectedProject = await onFilteredListRef(imageList);
-    window.location.href = `/project/${selectedProject}`;
-  };
+    const navigateToProject = async () => {
+       const selectedProject = await onFilteredListRef(imageList)
+       navigate(`/project/${selectedProject}`);
+        // window.location.href = `/project/${selectedProject}`;
+    }
 
-  return (
-    <div className="grid-item" onClick={navigateToProject}>
-      {imageList.length > 0 && (
-        <div
-          style={{
-            backgroundImage: `url(${imageList[currentImageIndex]})`,
-            height: '100%',
-            backgroundPosition: 'center',
-            backgroundSize: 'cover'
-          }}
-          className="project-image-container"
-        ></div>
-      )}
-    </div>
-  );
-};
+    return (
+        <button
+        onClick={navigateToProject}
+            className="grid-item"
+        >
+            {imageList.length > 0 && (
+                <div
+                    style={{
+                        backgroundImage: `url(${imageList[currentImageIndex]})`,
+                        height: '100%',
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover'
+                    }}
+                    className="project-image-container"
+                ></div>
+            )}
+        </button>
+    )
+}
 
-export default GridItem;
+export default GridItem
