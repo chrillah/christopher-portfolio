559078bafb9e0ac72112a59082ab24a8ef507e70
chrillah <ch.ronnberg@gmail.com>
2023-11-14 17:18:38 +0100

    fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,22 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 function HomePage() {
-  return (
-    <div className='menu-home'>
-                            <ul className="link-container">
-                        <li>
-                            <Link
-                                className="link-button"
-                                to="/about"
-                            >
-                                About
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                className="link-button"
-                                to="/projects"
-                            >
-                                Projects
-                            </Link>
-                        </li>
-                    </ul>
-    </div>
-  )
+    return (
+        <div className="menu-home">
+            <ul className="link-container">
+                <li>
+                    <Link className="link-button" to="/about">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link className="link-button" to="/projects">
+                        Projects
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default HomePage
