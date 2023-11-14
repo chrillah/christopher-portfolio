b8c44ed47d7c4219ca5203ce377e7f13eb44e58e
chrillah <ch.ronnberg@gmail.com>
2023-11-14 17:03:44 +0100

    update

--- src/pages/ProjectItem.tsx
+++ src/pages/ProjectItem.tsx
@@ -3,54 +3,92 @@ import { StorageReference } from '@firebase/storage'
 import PictureContainer from './PictureContainer'
 import imageListRefs from '../ImageListRefs'
 import myProjects from './myProjects'
+import { useEffect, useState } from 'react'
 
 interface ProjectData {
-    name: string;
-    description: string;
-    linkToWeb: string | null;
-    linkToGitHub: string;
-    technology: string[];
-  }
+    name: string
+    description: string
+    linkToWeb: string | null
+    linkToGitHub: string
+    technology: string[]
+}
 
 const ProjectItem = () => {
     const { project } = useParams<{ project: string | '' }>()
+    const [projectData, setProjectData] = useState<ProjectData | null>(null)
+    const [projectRef, setProjectRef] = useState<StorageReference | undefined>()
 
-    function findReferenceByPath(
-        searchPath: string
-    ): StorageReference | undefined {
-        if (imageListRefs) {
-            return imageListRefs.find((ref) =>
-                ref.fullPath.includes(searchPath)
-            )
+    useEffect(() => {
+        function findReferenceByPath(
+            searchPath: string
+        ): StorageReference | undefined {
+            if (imageListRefs) {
+                return imageListRefs.find((ref) =>
+                    ref.fullPath.includes(searchPath)
+                )
+            }
+            return undefined
         }
-        return undefined
-    }
 
-    let projectRef: StorageReference | undefined = undefined
+        let foundProjectRef: StorageReference | undefined = undefined
 
-    if (project) {
-        projectRef = findReferenceByPath(project)
-    }
+        if (project) {
+            foundProjectRef = findReferenceByPath(project)
+            setProjectRef(foundProjectRef)
+        }
 
-    const matchingProjectEntry = Object.entries(myProjects).find(
-        ([key]) => key === project
-      );
+        const matchingProjectEntry = Object.entries(myProjects).find(
+            ([key]) => key === project
+        )
 
-      const projectsToRender: Record<string, ProjectData> = matchingProjectEntry
-        ? { [matchingProjectEntry[0]]: matchingProjectEntry[1] }
-        : {};
+        if (matchingProjectEntry) {
+            setProjectData(matchingProjectEntry[1])
+        } else {
+            console.log('Inget matchande projekt hittades.')
+        }
+    }, [project])
+    // const [projectName, setProjectName] = useState('');
+    // const [description, setDescription] = useState('');
+    // const [linkToWeb, setLinkToWeb] = useState('');
+    // const [linkToGithub, setLinkToGithub] = useState('');
+    // const [technology, setTechnology] = useState([]);
 
-      if (Object.keys(projectsToRender).length > 0) {
-        const [projectKey, projectData] = Object.entries(projectsToRender)[0];
-        console.log(projectKey)
-        console.log(`${projectData.name}`);
-        console.log(`${projectData.description}`);
-        console.log(`${projectData.linkToWeb}`);
-        console.log(`${projectData.linkToGitHub}`);
-        console.log(`${projectData.technology}`);
-      } else {
-        console.log('Inget matchande projekt hittades.');
-      }
+    // function findReferenceByPath(
+    //     searchPath: string
+    // ): StorageReference | undefined {
+    //     if (imageListRefs) {
+    //         return imageListRefs.find((ref) =>
+    //             ref.fullPath.includes(searchPath)
+    //         )
+    //     }
+    //     return undefined
+    // }
+
+    // let projectRef: StorageReference | undefined = undefined
+
+    // if (project) {
+    //     projectRef = findReferenceByPath(project)
+    // }
+
+    // const matchingProjectEntry = Object.entries(myProjects).find(
+    //     ([key]) => key === project
+    // )
+
+    // const projectsToRender: Record<string, ProjectData> = matchingProjectEntry
+    //     ? { [matchingProjectEntry[0]]: matchingProjectEntry[1] }
+    //     : {}
+
+    // if (Object.keys(projectsToRender).length > 0) {
+    //     const [projectKey, projectData] = Object.entries(projectsToRender)[0]
+    //     console.log(projectKey)
+    //     console.log(`${projectData.name}`)
+    //     console.log(`${projectData.description}`)
+    //     console.log(`${projectData.linkToWeb}`)
+    //     console.log(`${projectData.linkToGitHub}`)
+    //     console.log(`${projectData.technology}`)
+    // } else {
+    //     console.log('Inget matchande projekt hittades.')
+    // }
 
     return (
         <div className="project-item-wrapper">
@@ -60,10 +98,70 @@ const ProjectItem = () => {
                     className="back-button"
                     onClick={() => console.log('back')}
                 >
-                    <div className="caret rev"></div>
-                    Back to Projects
+                    <div className="rev">
+                        <div className="caret"></div>
+                    </div>
+                    <p>Back</p>
                 </Link>
-                {projectRef && <PictureContainer imagesReference={projectRef} />}
+                {projectRef && (
+                    <PictureContainer imagesReference={projectRef} />
+                )}
+                <div className="project-info-wrapper">
+                    {projectData ? (
+                        <>
+                            <div className="project-info-container-1">
+                                <p className="project-name">
+                                    {projectData.name}
+                                </p>
+                                <p className="project-description">
+                                    {projectData.description}
+                                </p>
+                                {projectData.linkToWeb ? (
+                                    <>
+                                        <a
+                                            className="link-to-button"
+                                            target="blanc"
+                                            href={projectData.linkToWeb}
+                                        >
+                                            To webpage
+                                            <div className="no-rotate">
+                                                <div className="caret"></div>
+                                            </div>
+                                        </a>
+                                    </>
+                                ) : (
+                                    <>
+                                        {/* <p className="no-link">No webpage</p> */}
+                                    </>
+                                )}
+                            </div>
+                            <div className="project-info-container-2">
+                                <p className="technology-header">
+                                    Technologies and Languages
+                                </p>
+                                <div className="technology-list">
+                                    {projectData.technology.map(
+                                        (index, key) => (
+                                            <p key={key}>{index}</p>
+                                        )
+                                    )}
+                                </div>
+                                <a
+                                    className="link-to-button"
+                                    target="blanc"
+                                    href={projectData.linkToGitHub}
+                                >
+                                    GitHub
+                                    <div className="no-rotate">
+                                        <div className="caret"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </>
+                    ) : (
+                        <>nope</>
+                    )}
+                </div>
             </div>
         </div>
     )
