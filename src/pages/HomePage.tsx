6a620833b9871c031399a4e6fce04fd53a6cea6e
chrillah <ch.ronnberg@gmail.com>
2023-11-15 09:14:34 +0100

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 function HomePage() {
     return (
         <div className="menu-home">
+
+            {/* <h1 className='hero-title'>Christopher Rönnberg</h1> */}
             <ul className="link-container">
                 <li>
                     <Link className="link-button" to="/about">
