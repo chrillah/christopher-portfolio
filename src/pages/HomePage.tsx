2072c9dc000fffaf78dfb3eaa93517cc1ba34f93
chrillah <ch.ronnberg@gmail.com>
2023-11-15 23:09:36 +0100

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ function HomePage() {
             {/* <h1 className='hero-title-1'>Christopher Rönnberg</h1>
             <h1 className='hero-title-2'>{year.getFullYear()} Portfolio</h1> */}
 
-            <h1 className="hero-title">Christopher Rönnberg</h1>
+            <p className="hero-name">Christopher Rönnberg</p>
             <p className="hero-title-3">{year.getFullYear()} Portfolio</p>
             {/* <div className="gradient-bg"></div> */}
             <ul className="link-container">
